bc14e27733617b2d1ffaa2de0502960524f2e685
waldnershih <waldner0043@gmail.com>
2022-04-30 17:36:18 +1000

    init & add multifaces feature

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import './FaceRecognition.css';
+import React from "react";
+import "./FaceRecognition.css";
 
-const FaceRecognition = ({ imageUrl, box }) => {
-  return (
-    <div className='center ma'>
-      <div className='absolute mt2'>
-        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
-        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
-      </div>
-    </div>
-  );
-}
+const FaceRecognition = ({ imageUrl, boxes }) => {
+	const renderedBoxes = boxes.map((box, index) => {
+		return (
+			<div
+				className="bounding-box"
+				key={index}
+				style={{
+					top: box.topRow,
+					right: box.rightCol,
+					bottom: box.bottomRow,
+					left: box.leftCol,
+				}}
+			/>
+		);
+	});
+	return (
+		<div className="center ma">
+			<div className="absolute mt2">
+				<img
+					id="inputimage"
+					alt=""
+					src={imageUrl}
+					width="500px"
+					heigh="auto"
+				/>
+				{renderedBoxes}
+			</div>
+		</div>
+	);
+};
 
 export default FaceRecognition;
