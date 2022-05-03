6ca0a8978deb84cd55918e6bfdfcc98a1699d9a2
waldnershih <waldner0043@gmail.com>
2022-05-03 18:57:16 +1000

    style update && feature for lambda function update

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
-      </div>
-      <div className='white f1'>
-        {entries}
-      </div>
-    </div>
-  );
-}
+    const [emoji, setEmoji] = useState("");
+
+    useEffect(() => {
+        generateEmoji(entries);
+    }, [entries]);
+
+    const generateEmoji = (entries) => {
+        fetch(
+            `https://tmv1lusue2.execute-api.us-east-1.amazonaws.com/rank?rank=${entries}`
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                setEmoji(data.emoji);
+            })
+            .catch((err) => console.log(err));
+    };
+
+    return (
+        <div>
+            <div className="white f3">
+                {`${name}, your current entry count is...`}
+            </div>
+            <div className="white f1">{entries}</div>
+            <div className="white f3">{`Rank Badge: ${emoji}`}</div>
+        </div>
+    );
+};
 
 export default Rank;
